7c8156cc69d4e9d4706aa8517e80246f900503fd
ldengra <hiker_luis@hotmail.com>
2020-02-12 22:54:26 +0100

    ejercicio7

--- src/main/java/Ejercicio05/Main.java
+++ src/main/java/Ejercicio05/Main.java
@@ -1,8 +1,23 @@
 package Ejercicio05;
-
+import java.util.Scanner;
 public class Main {
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
 
 
+    }
+
+    public static String rellenarNombre(){
+
+        String nombre = "";
+        boolean nombreValido;
+        do{
+            try{
+                System.out.println("Inserta el nombre del alumno: ");
+                nombre = input.nextLine();
+            }catch ()
+        }while (!nombreValido);
+
+        return nombre;
     }
 }
---
+++ src/main/java/Ejercicio07/Main.java
@@ -0,0 +1,7 @@
+package Ejercicio07;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
